e8534666b1442d6a88b4c6eb3f4b3cc1e72102ac
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2020-10-04 21:29:29 +0800

    Working on react-animated-slider

--- assets/page/user/home/index.tsx
+++ assets/page/user/home/index.tsx
@@ -1,12 +1,20 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import React from "react";
+import Hero from "./hero";
 
+const useStyles = makeStyles((theme: Theme) => {
+	return createStyles({
+		root: {
+			display: "flex",
+			flexDirection: "column"
+		},
+	});
+});
 const Home:React.FC<IBlankMethod> = () => {
+	const classes = useStyles();
 	return (
-		<div>
-			This is home
-			<button>
-				Add Some thing
-			</button>
+		<div className={classes.root}>
+			<Hero />
 		</div>
 	);
 };
